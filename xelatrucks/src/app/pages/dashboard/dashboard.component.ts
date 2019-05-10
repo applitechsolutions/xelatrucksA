86b9243e06eba773e1e9161b749c028ee35a817f
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-05-09 20:37:40 -0600

    ** PRIMER COMMIT **

---
+++ xelatrucks/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styles: ['../../app.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

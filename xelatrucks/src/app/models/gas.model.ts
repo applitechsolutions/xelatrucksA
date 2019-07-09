3ec0df3cc916316cf61394718d5e87c00ed22b74
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-07-08 19:22:04 -0600

    gasolines 1.0

---
+++ xelatrucks/src/app/models/gas.model.ts
@@ -0,0 +1,12 @@
+
+export class Gas {
+
+    constructor(
+        public date: string,
+        public gallons: number,
+        public total: number,
+        public code?: string,
+        public _id?: string
+    ) {}
+
+}

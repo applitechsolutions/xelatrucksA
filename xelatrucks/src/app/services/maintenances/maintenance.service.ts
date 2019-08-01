fb8c5b8bfdfae3607b60ed1b188d8c41dbb9a28b
LnkColumbus <marlonveliz.nero19@gmail.com>
2019-08-01 12:07:29 -0600

    LISTADO DE MANTENIMIENTOS

--- xelatrucks/src/app/services/maintenances/maintenance.service.ts
+++ xelatrucks/src/app/services/maintenances/maintenance.service.ts
@@ -35,6 +35,12 @@ export class MaintenanceService {
       }));
   }
 
+  cargarTerminados() {
+    const url = URL_SERVICES + '/mantenimiento/terminados';
+
+    return this.http.get(url);
+  }
+
   crearMantenimiento( mantenimiento: Maintenance ) {
 
     let url = URL_SERVICES + '/mantenimiento';
